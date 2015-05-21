bd2891acd7a5bcb167d95bf496830f60b35e117d
filipnaumovic <filip@salsitasoft.com>
2015-05-21 17:16:59 +0200

    fixed css imports

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,16 @@ var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var rename = require("gulp-rename");
 var postcss = require('gulp-postcss');
-var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('autoprefixer-core');
-var minifycss = require('gulp-minify-css');
 
 gulp.task('build-styles', function () {
   gulp.src('./src/css/_main.styl')
     .pipe(stylus({
-      compress: true
+      compress: true,
+      'include css': true
     }))
     .pipe(rename('gonzo.css'))
-    .pipe(sourcemaps.init())
     .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
-    .pipe(sourcemaps.write('.'))
-    .pipe(minifycss({compatibility: 'ie8'}))
     .pipe(gulp.dest('./build'));
 });
 
