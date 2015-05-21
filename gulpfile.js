db9d049022b1205f05f9825466b3062520884810
filipnaumovic <filip@salsitasoft.com>
2015-05-21 16:18:53 +0200

    added watcher

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,23 @@ var rename = require("gulp-rename");
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('autoprefixer-core');
-var minifyCss = require('gulp-minify-css');
+var minifycss = require('gulp-minify-css');
 
-gulp.task('compile-stylus', function () {
+gulp.task('build-styles', function () {
   gulp.src('./src/css/main.styl')
     .pipe(stylus({
       compress: true
     }))
     .pipe(rename('gonzo.css'))
-    .pipe(minifyCss({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./build'));
-});
-
-gulp.task('autoprefixer', function () {
-  gulp.src('./build/*.css')
     .pipe(sourcemaps.init())
     .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
     .pipe(sourcemaps.write('.'))
+    .pipe(minifycss({compatibility: 'ie8'}))
     .pipe(gulp.dest('./build'));
 });
 
-gulp.task('default', ['compile-stylus', 'autoprefixer']);
+gulp.task('watch', function() {
+  gulp.watch('./src/css/**/*.styl', ['build-styles']);
+});
+
+gulp.task('default', ['build-styles', 'watch']);
