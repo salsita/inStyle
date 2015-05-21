b50a9c90716c71ece274d441d6ef6d1ba2fbf78d
filipnaumovic <filip@salsitasoft.com>
2015-05-21 15:19:28 +0200

    changed structure, added gulp build, autoprefixer

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+var rename = require("gulp-rename");
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer-core');
+
+gulp.task('compile-stylus', function () {
+  gulp.src('./src/css/main.styl')
+    .pipe(stylus({
+      compress: true
+    }))
+    .pipe(rename('gonzo.css'))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('autoprefixer', function () {
+  gulp.src('./build/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('default', ['compile-stylus', 'autoprefixer']);
