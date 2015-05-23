46efe5e07463538975d57dad72d7854d1bf5a710
filipnaumovic <filip@salsitasoft.com>
2015-05-23 19:58:02 +0200

    iconfont builder, distributes (inline, flex), improved structure, fixes

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,32 @@ var stylus = require('gulp-stylus');
 var rename = require("gulp-rename");
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer-core');
+var consolidate = require("gulp-consolidate");
+var minifyCss = require('gulp-minify-css');
+var iconfont = require('gulp-iconfont');
+var lodash = require('lodash');
+
+var fontName = 'icons';
+var fontPath = 'assets/font/'
+
+gulp.task('build-iconfont', function () {
+  gulp.src(['./src/components/icons/*.svg'])
+    .pipe(iconfont({
+      fontName: fontName,
+      normalize: true
+    }))
+      .on('codepoints', function(codepoints) {
+        gulp.src('./src/components/icons/template.styl')
+          .pipe(consolidate('lodash', {
+            glyphs: codepoints,
+            fontName: fontName,
+            fontPath: fontPath
+          }))
+          .pipe(rename({ basename: fontName }))
+          .pipe(gulp.dest('./src/components/'));
+    })
+  .pipe(gulp.dest('./build/'+fontPath));
+});
 
 gulp.task('build-styles', function () {
   gulp.src('./src/main.styl')
@@ -12,11 +38,12 @@ gulp.task('build-styles', function () {
     }))
     .pipe(rename('gonzo.css'))
     .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
-    .pipe(gulp.dest('./build'));
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./build/'));
 });
 
 gulp.task('watch', function() {
   gulp.watch('./src/**/*.styl', ['build-styles']);
 });
 
-gulp.task('default', ['build-styles', 'watch']);
+gulp.task('default', ['build-iconfont', 'build-styles', 'watch']);
