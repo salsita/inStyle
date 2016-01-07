7081e2312e79e068bebb9bb12d6428d552948a94
mstrdt <mystrdat@gmail.com>
2016-01-07 02:27:29 +0100

    new era

--- src/stylus/components/chili/js/str-index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function() {
-  return function(stylus) {
-    stylus.define('str-index', function(substr, str) {
-      return new stylus.nodes.Unit(str.string.indexOf(substr.string));
-    });
-  };
-};
--- src/stylus/components/chili/js/str-replace.js
+++
@@ -1,7 +0,0 @@
-module.exports = function() {
-  return function(stylus) {
-    stylus.define('str-replace', function(repl, substr, str) {
-      return new stylus.nodes.Unit(str.string.replace(repl.string, substr.string));
-    });
-  };
-};
